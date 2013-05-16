8018874385cb66f9011dc3ed4bc32b31e9148a68
p4u <p4u@dabax.net>
2013-05-16 19:03:19 +0200

    Add copyright

--- micmp.c
+++ micmp.c
@@ -1,4 +1,19 @@
+/*
+    Copyright (C) 2013 Pau Escrich
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
 #include <stdint.h>
@@ -8,8 +23,7 @@
 #include <fcntl.h>
 #include <pcap.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
-#define CONFIG "/etc/magicicmp.conf"
+#include <netinet/in.h>#define CONFIG "/etc/magicicmp.conf"
 
 static const char *filter = "icmp6";
 
