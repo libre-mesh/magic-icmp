92744523bd286521e6325c3a8fa5bdaae983cd83
p4u <p4u@dabax.net>
2013-07-15 23:34:03 +0200

    Add send.sh script to send PING6 magic-icmp messages

---
+++ send.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+help() {
+	echo "Usage: $0 <ip_dest> <cmd_type> [data]"
+	echo "Example: $0 ff02::1%eth0 004a 77777777"
+	echo "Happy hakking ;)"
+	exit 0
+}
+
+PING="ping6"
+FILTER="8888"
+SIZE=24
+IP="$1"
+CMD="$2"
+DATA="$3"
+
+[ -z "$CMD" ] && help
+
+$PING -p ${FILTER}${CMD}${DATA} ${IP} -s ${SIZE} -c1
